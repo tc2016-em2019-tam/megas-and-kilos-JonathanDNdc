45e88e3f5499929351cbf01874d9adbdf6e05b39
JonathanDNdc <A01411277@itesm.mx>
2019-01-18 20:19:58 -0600

    first commit

---
+++ src/com/jdndc/Main.java
@@ -0,0 +1,20 @@
+package com.jdndc;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        MegabyteConverter.printMegaBytesAndKiloBytes(4100);
+        MegabyteConverter.printMegaBytesAndKiloBytes(2055);
+        MegabyteConverter.printMegaBytesAndKiloBytes(3072);
+
+
+        //MegabyteConverter.printMegaBytesAndKiloBytes(-11);
+        //MegabyteConverter.printMegaBytesAndKiloBytes(0);
+
+        //MegabyteConverter mb = new MegabyteConverter(5);
+        //mb.setKilobytes(-12);
+
+        //MegabyteConverter mb1 = new MegabyteConverter(-13);
+    }
+}
